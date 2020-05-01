02e6e6b9cd3bd04e0a40014fde91f39b11f76930
idkup <51726427+idkup@users.noreply.github.com>
2020-05-01 02:44:05 -0400

    Prevent future key leaks

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 from discord.ext import commands
-KEY = 'NjQ3MzI4NjUyNzQ4MzkwNDAx.XdeF2g.jBRvl4PLRMAh-zAXTFZEqd7p7-g'
+with open('key.txt') as key_file:
+    KEY = key_file.readline()
+    key_file.close()
 bot = commands.Bot(command_prefix='_')
+bot.run(KEY)
 
