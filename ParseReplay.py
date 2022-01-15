805641722e98106226f3d204fd76e26208c90f09
idkup <idkup137@gmail.com>
2022-01-15 14:10:35 -0500

    fix bugs relating to leech seed and perish song

--- ParseReplay.py
+++ ParseReplay.py
@@ -134,13 +134,16 @@ def parse_replay(msg):
             elif ls[3] == "Hail" or ls[3] == "Sandstorm":
                 damaging_weather = (attacker, attacking_player)
             elif ls[3] == "Leech Seed":
-                defender.seeded = attacker
+                try:
+                    defender.seeded = attacker
+                except AttributeError:
+                    pass
 
             i = line + 1
             while log[i] != '|' and "|move|" not in log[i]:
                 if "|0 fnt" in log[i] and "|[from] recoil" not in log[i]:
                     attacker.direct_kills += 1
-                elif "|-status|" in log[i] and "move: Rest" not in log[i] or "|-start|" in log[i] and "Substitute" not in log[i]:
+                elif "|-status|" in log[i] and ("move: Rest" not in log[i] and "move: Perish Song" not in log[i]) or "|-start|" in log[i] and "Substitute" not in log[i]:
                     try:
                         defender.status_induced = attacker
                     except AttributeError:
