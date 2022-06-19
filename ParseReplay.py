65639c15edc154374572651a6fa0d8b2b223ae31
idkup <idkup137@gmail.com>
2022-06-19 19:38:48 -0400

    try fixing urshifu again

--- ParseReplay.py
+++ ParseReplay.py
@@ -29,10 +29,8 @@ def parse_replay(msg):
                         p = "Silvally"
                     elif p == "Arceus-*":
                         p = "Arceus"
-                    elif p == "Urshifu-Rapid-Strike":
-                        p = "Urshifu-Rapid"
-                    elif p == "Urshifu-Single-Strike":
-                        p = "Urshifu-Single"
+                    elif p == "Urshifu-Rapid-Strike" or p == "Urshifu-Single-Strike":
+                        p = "Urshifu"
                     p1.team.append(BattlePokemon(p))
             elif '|p2|' in l:
                 try:
